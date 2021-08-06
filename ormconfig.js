4e87f08bd14c8ae83e76edd376b75d0594c83e13
hamandakuntz <hamanda.monteiro@hotmail.com>
2021-08-06 15:02:11 -0300

    config ssl

--- ormconfig.js
+++ ormconfig.js
@@ -6,6 +6,10 @@ module.exports = {
   migrationsTableName: "migrations",
   entities: ["dist/entities/*.js"],
   migrations: ["dist/migrations/*.js"],
+  extra: {
+    ssl:{
+      rejectUnauthorized:false
+    }},
   cli: {
     migrationsDir: "src/migrations",
     entitiesDir: "dist/entities/*.js"
